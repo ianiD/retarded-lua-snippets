630c16cb3fed97f3361793b399ab4d3607d36e76
Dragomir Ioan <ianiD@users.noreply.github.com>
2016-04-30 14:37:44 +0300

    Retarded me

--- switch.lua
+++ switch.lua
@@ -11,7 +11,7 @@ Example code:
 	local switch = require("switch")
 
 	print("Do you want to play a game?")
-	switch(io.read)
+	switch(io.read())
 	:case("yes"):case("Yes"):case("YES"):case("y"):run(function() print("Good!") end)
 	:case("no"):case("No"):case("NO"):case("n"):run(function() print("Wrong answer!") end)
 	:default(function() print("wut?") end)
